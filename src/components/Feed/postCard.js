9bb84bd6e583328864121b3ab3bd768d2f81c3b0
Victor Wang <victorwang2001@gmail.com>
2021-12-16 19:24:23 -0500

    images

--- src/components/Feed/postCard.js
+++ src/components/Feed/postCard.js
@@ -1,4 +1,5 @@
-import { Card, Dropdown, DropdownButton, Container } from "react-bootstrap";
+import { useState, useEffect } from 'react';
+import { Card, Dropdown } from "react-bootstrap";
 
 function PostCard (props) {
   const id = props.id;
@@ -7,8 +8,9 @@ function PostCard (props) {
   const title = props.title;
   const deletePost = props.deletePost;
   const currentUser = props.currentUser;
+  const [postContent, setPostContent] = useState(content);
 
-  function handleDeletePost(e) {
+  function handleDeletePost() {
     deletePost(id);
   }
 
@@ -23,13 +25,45 @@ function PostCard (props) {
 
   const headWithoutDropdown = (<> {title} - @{username} </>);
 
+  useEffect(() => {
+    // placing this here so that this code is never run after the first render (none of the props should ever change)
+    // and increases performance
+    function replaceImageUrlWithImgTag(inputStr) {
+      const str = inputStr + " "; // adding a whitespace at the end to make sure we don't miss the last image
+      const urlRegex = /(https?:\/\/[^\s]+\.(?:png|jpg|jpeg|gif)(\?([a-zA-Z0-9_]*=[a-zA-Z0-9_]*)(\&[a-zA-Z0-9_]*=[a-zA-Z0-9_]*)*)?\s)/g;
+      const allMatches = [...str.matchAll(urlRegex)];
+      if (allMatches.length === 0) {
+        return str;
+      }
+
+      const allIndexes = allMatches.map(match => match.index);
+      const allMatchText = allMatches.map(match => match[0]);
+      const allMatchTextLength = allMatchText.map(text => text.length);
+
+      let blocks = [];
+      let currInd = 0;
+      allIndexes.forEach((ind, i) => {
+        blocks.push(str.substring(currInd, ind));
+        blocks.push(<img className="post-embedded-image" key={i} src={allMatchText[i]} />);
+        currInd = ind + allMatchTextLength[i];
+      });
+
+      blocks.push(str.substring(currInd));
+
+      return blocks;
+    }
+
+    setPostContent(replaceImageUrlWithImgTag(content));
+  }, []);
+
+
   return (
     <Card className="my-2">
       <Card.Header> 
         {currentUser === username ? headWithDropdown : headWithoutDropdown}
       </Card.Header>
       <Card.Body>
-        <Card.Text>{content}</Card.Text>
+        <Card.Text>{postContent} </Card.Text>
       </Card.Body>
     </Card>
   );
